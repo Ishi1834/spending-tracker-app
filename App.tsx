bf2f80625a661aa5a74893c061ed7befe897bcb3
Ismail Mohamad <ikmohamad19@gmail.com>
2024-01-07 23:30:13 +0000

    feat: add react native paper theme to react navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,34 @@
 import "react-native-gesture-handler"
-import { NavigationContainer } from "@react-navigation/native"
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from "@react-navigation/native"
+import merge from "deepmerge"
 import Constants from "expo-constants"
-import { PaperProvider } from "react-native-paper"
+import { PaperProvider, adaptNavigationTheme } from "react-native-paper"
+import { MD3DarkTheme, MD3LightTheme } from "react-native-paper"
 import Storybook from "./.storybook"
 import Main from "./src/screens"
 
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: NavigationDefaultTheme,
+  reactNavigationDark: NavigationDarkTheme,
+})
+
+const CombinedDefaultTheme = merge(MD3LightTheme, LightTheme)
+const CombinedDarkTheme = merge(MD3DarkTheme, DarkTheme)
+
 const App = () => {
+  const isDarkTheme = false
+
   return (
-    <PaperProvider>
-      <NavigationContainer>
+    <PaperProvider
+      theme={isDarkTheme ? CombinedDarkTheme : CombinedDefaultTheme}
+    >
+      <NavigationContainer
+        theme={isDarkTheme ? CombinedDarkTheme : CombinedDefaultTheme}
+      >
         <Main />
       </NavigationContainer>
     </PaperProvider>
