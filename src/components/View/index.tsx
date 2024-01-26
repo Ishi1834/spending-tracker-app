41e24e9c99475e0f34dc4d98d1a15e26e6387cde
Ismail Mohamad <ikmohamad19@gmail.com>
2024-01-26 16:49:09 +0000

    feat: add style extension object to view component

--- src/components/View/index.tsx
+++ src/components/View/index.tsx
@@ -1,19 +1,23 @@
-import { View as RNView, StyleSheet } from "react-native"
+import { View as RNView, StyleSheet, ViewStyle } from "react-native"
 import { useTheme } from "react-native-paper"
 
 export type ViewProps = {
   children: React.ReactNode
+  styleExtension?: {
+    [key in keyof ViewStyle]?: ViewStyle[key]
+  }
 }
 
 /**
  * A View component with margin, padding and react-native-paper's theme applied.
  */
-export const View = ({ children }: ViewProps) => {
+export const View = ({ children, styleExtension }: ViewProps) => {
   const theme = useTheme()
   return (
     <RNView
       style={{
         ...styles.container,
+        ...styleExtension,
         backgroundColor: theme.colors.background,
       }}
     >
