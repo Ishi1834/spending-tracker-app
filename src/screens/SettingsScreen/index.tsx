07940a0ad3e820c0e53bba34a613b17795861833
Ismail Mohamad <ikmohamad19@gmail.com>
2024-01-26 17:39:23 +0000

    feat: add notification toggle

--- src/screens/SettingsScreen/index.tsx
+++ src/screens/SettingsScreen/index.tsx
@@ -1,5 +1,6 @@
-/* eslint-disable react-native/no-inline-styles */
 import { useState, useEffect } from "react"
+import { StyleSheet } from "react-native"
+import { Divider } from "react-native-paper"
 import { View, Text, SwitchInput } from "../../components"
 import { isAuthRequired, toggleAuthRequired } from "../../utils/auth"
 
@@ -12,17 +13,50 @@ export const SettingsScreen = () => {
 
   return (
     <View>
-      <Text variant="titleMedium">Permissions</Text>
-      <View styleExtension={{ marginHorizontal: 0 }}>
-        <SwitchInput
-          icon="lock"
-          label="Enable device authentication"
-          isSwitchOn={isAuthEnabled}
-          onSwitchToggle={() =>
-            toggleAuthRequired().then((val) => setIsAuthEnabled(val))
-          }
-        />
+      <View styleExtension={styles.groupWrapper}>
+        <Text variant="titleMedium">Security</Text>
+        <View styleExtension={styles.inputWrapper}>
+          <SwitchInput
+            icon="lock"
+            label="Enable device authentication"
+            isSwitchOn={isAuthEnabled}
+            onSwitchToggle={() =>
+              toggleAuthRequired().then((val) => setIsAuthEnabled(val))
+            }
+          />
+        </View>
+      </View>
+      <Divider style={styles.divider} />
+      <View styleExtension={styles.groupWrapper}>
+        <Text variant="titleMedium">Permissions</Text>
+        <View styleExtension={styles.inputWrapper}>
+          <SwitchInput
+            icon="bell"
+            label="Allow notifications"
+            isSwitchOn={false}
+            onSwitchToggle={() =>
+              console.log("Allow notifications switch toggled")
+            }
+          />
+        </View>
       </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  groupWrapper: {
+    marginHorizontal: 0,
+    flex: 0,
+    marginVertical: 0,
+  },
+  inputWrapper: {
+    marginHorizontal: 0,
+    marginVertical: 0,
+    marginTop: 5,
+    flex: 0,
+  },
+  divider: {
+    marginVertical: 15,
+  },
+})
