1208b8604931608eba65c540fc7fa37821ffe0f6
Ismail Mohamad <ikmohamad19@gmail.com>
2024-02-25 16:03:05 +0000

    feat: save transaction to db when submit is clicked

--- src/screens/AddTransactionScreen/index.tsx
+++ src/screens/AddTransactionScreen/index.tsx
@@ -1,4 +1,6 @@
 import { yupResolver } from "@hookform/resolvers/yup"
+import { ParamListBase } from "@react-navigation/native"
+import { StackNavigationProp } from "@react-navigation/stack"
 import { useEffect, useState } from "react"
 import { useForm, Controller } from "react-hook-form"
 import { StyleSheet, Image, ScrollView } from "react-native"
@@ -12,9 +14,10 @@ import {
   View,
   Button,
   HelperText,
+  Snackbar,
 } from "../../components/"
-import { Category } from "../../types"
-import { getCategories } from "../../utils/database"
+import { Category, TransactionFormData } from "../../types"
+import { getCategories, addTransaction } from "../../utils/database"
 
 const schema = yup.object().shape({
   category: yup.number().integer().required("Category is required"),
@@ -25,12 +28,20 @@ const schema = yup.object().shape({
     .positive("Amount should be greater than 0")
     .required("Amount is required"),
   date: yup.date().required("Date is required"),
-  expense: yup.number().oneOf([0, 1]).required(), // 0 for expense, 1 for income
+  expense: yup.string().oneOf(["True", "False"]).required(), // 0 for expense, 1 for income
 })
 
-export const AddTransactionScreen = () => {
+type AddTransactionScreenProps = {
+  navigation: StackNavigationProp<ParamListBase, "AddATransaction", undefined>
+}
+
+export const AddTransactionScreen = ({
+  navigation,
+}: AddTransactionScreenProps) => {
   const [categories, setCategories] = useState<Category[]>([])
   const [isCategoriesPickerOpen, setIsCategoriesPickerOpen] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isShowingError, setIsShowingError] = useState(false)
 
   useEffect(() => {
     getCategories()
@@ -42,151 +53,170 @@ export const AddTransactionScreen = () => {
     control,
     handleSubmit,
     formState: { errors },
-  } = useForm({
+  } = useForm<TransactionFormData>({
     resolver: yupResolver(schema),
     defaultValues: {
       description: "",
       amount: 0,
-      expense: 0,
+      expense: "True",
     },
   })
-  const onSubmit = (data) => console.log(data)
 
-  //console.log(errors)
+  const onSubmit = (data: TransactionFormData) => {
+    setIsLoading(true)
+    addTransaction(data)
+      .then(() => {
+        setIsLoading(false)
+        navigation.goBack()
+      })
+      .catch(() => {
+        setIsLoading(false)
+        setIsShowingError(true)
+      })
+  }
+
   return (
-    <ScrollView>
-      <ScreenWrapper styleExtension={styles.container}>
-        <Image
-          source={require("../../../assets/adaptive-icon.png")}
-          style={styles.image}
-        />
-        <Controller
-          control={control}
-          render={({ field: { onChange, value, onBlur } }) => (
-            // eslint-disable-next-line react-native/no-inline-styles
-            <View styleExtension={{ ...styles.inputWrapper, zIndex: 999 }}>
-              <Picker<Category>
-                open={isCategoriesPickerOpen}
-                setOpen={setIsCategoriesPickerOpen}
-                schema={{
-                  label: "category_name",
-                  value: "category_id",
-                }}
-                items={categories}
-                value={value}
-                // Temporary fix, as setValue expects a react state setter function
-                setValue={() => ({})}
-                onSelectItem={(val) => onChange((val as Category).category_id)}
-                onClose={onBlur}
-                multiple={false}
-                placeholder="Select a category"
-                listMode="SCROLLVIEW"
-                zIndex={999}
-                error={Boolean(errors.category)}
-              />
-              <HelperText type="error" visible={Boolean(errors.category)}>
-                {errors.category?.message}
-              </HelperText>
-            </View>
-          )}
-          name="category"
-        />
-        <Controller
-          control={control}
-          name="description"
-          render={({ field: { onChange, value, onBlur } }) => (
-            <View styleExtension={styles.inputWrapper}>
-              <TextInput
-                style={styles.input}
-                mode="outlined"
-                label="Description"
-                onChangeText={onChange}
-                value={value}
-                onBlur={onBlur}
-                error={Boolean(errors.description)}
-              />
-              <HelperText type="error" visible={Boolean(errors.description)}>
-                {errors.description?.message}
-              </HelperText>
-            </View>
-          )}
-        />
-        <Controller
-          control={control}
-          name="amount"
-          render={({ field: { onChange, value, onBlur } }) => (
-            <View styleExtension={styles.inputWrapper}>
-              <TextInput
-                style={styles.input}
-                mode="outlined"
-                label="Amount"
-                onChangeText={onChange}
-                inputMode="numeric"
-                value={value?.toString()}
-                onBlur={onBlur}
-                error={Boolean(errors.amount)}
-              />
-              <HelperText type="error" visible={Boolean(errors.amount)}>
-                {errors.amount?.message}
-              </HelperText>
-            </View>
-          )}
-        />
-        <Controller
-          control={control}
-          name="date"
-          render={({ field: { onChange, value, onBlur } }) => (
-            <View styleExtension={styles.inputWrapper}>
-              <DatePicker
-                label="Transaction Date"
-                mode="outlined"
-                value={value}
-                onChange={onChange}
-                inputMode="start"
-                withDateFormatInLabel={false}
-                style={styles.input}
-                onBlur={onBlur}
-                hideValidationErrors={true}
-                placeholder="MM/DD/YYYY"
-                onValidationError={(error) =>
-                  error && control.setError("date", { message: error })
-                }
-                error={Boolean(errors.date)}
-                hasError={Boolean(errors.date)}
-              />
-              <HelperText type="error" visible={Boolean(errors.date)}>
-                {errors.date?.message}
-              </HelperText>
-            </View>
-          )}
-        />
+    <>
+      <ScrollView>
+        <ScreenWrapper styleExtension={styles.container}>
+          <Image
+            source={require("../../../assets/adaptive-icon.png")}
+            style={styles.image}
+          />
+          <Controller
+            control={control}
+            render={({ field: { onChange, value, onBlur } }) => (
+              // eslint-disable-next-line react-native/no-inline-styles
+              <View styleExtension={{ ...styles.inputWrapper, zIndex: 999 }}>
+                <Picker<Category>
+                  open={isCategoriesPickerOpen}
+                  setOpen={setIsCategoriesPickerOpen}
+                  schema={{
+                    label: "category_name",
+                    value: "category_id",
+                  }}
+                  items={categories}
+                  value={value}
+                  // Temporary fix, as setValue expects a react state setter function
+                  setValue={() => ({})}
+                  onSelectItem={(val) =>
+                    onChange((val as Category).category_id)
+                  }
+                  onClose={onBlur}
+                  multiple={false}
+                  placeholder="Select a category"
+                  listMode="SCROLLVIEW"
+                  zIndex={999}
+                  error={Boolean(errors.category)}
+                />
+                <HelperText type="error" visible={Boolean(errors.category)}>
+                  {errors.category?.message}
+                </HelperText>
+              </View>
+            )}
+            name="category"
+          />
+          <Controller
+            control={control}
+            name="description"
+            render={({ field: { onChange, value, onBlur } }) => (
+              <View styleExtension={styles.inputWrapper}>
+                <TextInput
+                  style={styles.input}
+                  mode="outlined"
+                  label="Description"
+                  onChangeText={onChange}
+                  value={value}
+                  onBlur={onBlur}
+                  error={Boolean(errors.description)}
+                />
+                <HelperText type="error" visible={Boolean(errors.description)}>
+                  {errors.description?.message}
+                </HelperText>
+              </View>
+            )}
+          />
+          <Controller
+            control={control}
+            name="amount"
+            render={({ field: { onChange, value, onBlur } }) => (
+              <View styleExtension={styles.inputWrapper}>
+                <TextInput
+                  style={styles.input}
+                  mode="outlined"
+                  label="Amount"
+                  onChangeText={onChange}
+                  inputMode="numeric"
+                  value={value?.toString()}
+                  onBlur={onBlur}
+                  error={Boolean(errors.amount)}
+                />
+                <HelperText type="error" visible={Boolean(errors.amount)}>
+                  {errors.amount?.message}
+                </HelperText>
+              </View>
+            )}
+          />
+          <Controller
+            control={control}
+            name="date"
+            render={({ field: { onChange, value, onBlur } }) => (
+              <View styleExtension={styles.inputWrapper}>
+                <DatePicker
+                  label="Transaction Date"
+                  mode="outlined"
+                  value={value}
+                  onChange={onChange}
+                  inputMode="start"
+                  withDateFormatInLabel={false}
+                  style={styles.input}
+                  onBlur={onBlur}
+                  hideValidationErrors={true}
+                  placeholder="MM/DD/YYYY"
+                  onValidationError={(error) =>
+                    error && control.setError("date", { message: error })
+                  }
+                  error={Boolean(errors.date)}
+                  hasError={Boolean(errors.date)}
+                />
+                <HelperText type="error" visible={Boolean(errors.date)}>
+                  {errors.date?.message}
+                </HelperText>
+              </View>
+            )}
+          />
 
-        <Controller
-          control={control}
-          name="expense"
-          render={({ field: { onChange, value } }) => (
-            <View styleExtension={styles.radioGroupWrapper}>
-              <RadioButton.Group
-                onValueChange={(val) => onChange(parseInt(val))}
-                value={value?.toString()}
-              >
-                <View styleExtension={styles.radioGroup}>
-                  <RadioButton.Item label="Expense" value="0" />
-                  <RadioButton.Item label="Income" value="1" />
-                </View>
-              </RadioButton.Group>
-            </View>
-          )}
-        />
-        <Button
-          mode="contained"
-          style={styles.button}
-          onPress={handleSubmit(onSubmit)}
-        >
-          Save Transaction
-        </Button>
-      </ScreenWrapper>
-    </ScrollView>
+          <Controller
+            control={control}
+            name="expense"
+            render={({ field: { onChange, value } }) => (
+              <View styleExtension={styles.radioGroupWrapper}>
+                <RadioButton.Group onValueChange={onChange} value={value}>
+                  <View styleExtension={styles.radioGroup}>
+                    <RadioButton.Item label="Expense" value="True" />
+                    <RadioButton.Item label="Income" value="False" />
+                  </View>
+                </RadioButton.Group>
+              </View>
+            )}
+          />
+          <Button
+            mode="contained"
+            style={styles.button}
+            onPress={handleSubmit(onSubmit)}
+            loading={isLoading}
+          >
+            Save Transaction
+          </Button>
+        </ScreenWrapper>
+      </ScrollView>
+      <Snackbar
+        visible={isShowingError}
+        onDismiss={() => setIsShowingError(false)}
+      >
+        Error adding transaction. Please try again.
+      </Snackbar>
+    </>
   )
 }
 
